5aefb43aebc2a6c925e4b1bab6977900f88fce26
Hamish <trust-no-1@adam.com.au>
2016-04-16 14:42:54 +0930

    Simple beginnings

---
+++ src/Kode_Unity/KodeUnity.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Kode_Unity
+{
+    public class KodeUnity : PartModule
+    {
+        private Rect WindowPosition = new Rect();
+
+        public override void OnStart(StartState state)
+        {
+            if (state != StartState.Editor)
+                RenderingManager.AddToPostDrawQueue(0, OnDraw);
+        }
+
+        private void OnDraw()
+        {
+            if (this.vessel == FlightGlobals.ActiveVessel)
+                WindowPosition = GUILayout.Window(10, WindowPosition, OnWindow, "This is a title");
+        }
+
+        private void OnWindow(int windowId)
+        {
+            GUILayout.BeginHorizontal(GUILayout.Width(250f));
+            GUILayout.Label("This is a label");
+            GUILayout.EndHorizontal();
+
+            GUI.DragWindow();
+        }
+    }
+}
